e87c55a298079736a57fbb6b239acb3f63772555
Sheru20000 <hritikmishra91@gmail.com>
2024-03-03 12:52:35 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://robohash.org/vfdbd" className="App-logo" alt="logo" />
+        <h2>Robo's Threat</h2>
+        <a
+          className="App-link"
+          href="https://robohash.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >  Purchase The Robos       
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
